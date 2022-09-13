e4548cd1ccc1d52ad8b1c2882243d3aa9e9d0a33
Juan Gonzalez Covarrubias <104181238+jgonzalezcov@users.noreply.github.com>
2022-09-12 23:26:02 -0300

    The option to filter and order comics heroes is created. In addition, the visualization of its comics and series is added.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import logo from '../assets/img/marvel.png'
-import funnel from '../assets/img/embudomarvel.png'
 
 const Header = (props) => {
   return (
     <header>
       <img className="logo-header" src={logo} alt="Logo" />
       <div className="container-funnel">
-        <img className="funnel" src={funnel} alt="Funnel" />
-        <h3 className="text-funnel">Filtro activo</h3>
+        {props.filterActiv === true ? (
+          <h3 className="text-funnel">Visualización filtrada</h3>
+        ) : (
+          ''
+        )}
       </div>
-      <button
-        className="formIn header-button"
-        onClick={() => props.viewFilter()}
-      >
-        {props.filterView === true ? 'Ocultar filtros' : 'Ver filtros'}
-      </button>
+      <div className="container-header-button">
+        {props.filterView === false && props.viewMenuOder === false ? (
+          <button
+            className="formIn header-button"
+            onClick={() => props.viewFilter()}
+          >
+            Ver filtros
+          </button>
+        ) : (
+          ''
+        )}
+      </div>
     </header>
   )
 }
